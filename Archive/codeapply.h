2489a2092866d4bc49d65a0ade36e5581149e953
Long Nguyen <long.nguyen@tuni.fi>
2023-04-18 09:27:11 +0300

    Contents + updated README.md

---
+++ Archive/codeapply.h
@@ -0,0 +1,353 @@
+"e00s50s70s80s90s51s71s91s52s62s72s92"
+"e0fs5ds6ds7ds8ds9ds5es9es5fs6fs7fs8fs9f"
+"e0fs59s69s79s89s99s5as9as5bs6bs7bs8bs9b"
+"e0fs55s65s75s85s95s56s96s57s67s77s87s97"
+"e0fs50s60s70s80s90s51s91s52s62s72s82s92"
+"e70s68s88"
+
+
+
+// Number 0 of the first matrix
+"e0fs55s65s75s85s95s56s96s57s67s77s87s97"
+// Number 0 of the second matrix
+"e00s59s69s79s99s5as7as9as5bs7bs8bs9b"
+"e0fs59s69s79s89s99s5as9as5bs6bs7bs8bs9b"
+// Number 0 of the third matrix
+"e00s5ds6ds7ds9ds5es7es9es5fs6fs7fs8fs9f"
+"e0fs5ds6ds7ds8ds9ds5es9es5fs6fs7fs8fs9fw1DC4"
+// Number 0 of the fourth matrix
+
+"e00s5ds6ds7ds8ds9ds5es9es5fs6fs7fs8fs9f"
+"e0fs5fs6fs7fs8fs9fw1DC4"
+// Number 1 of fourth matrix
+
+"e00s5fs6fs7fs8fs9f"
+"e0fs5ds7ds8ds9ds5es7es9es5fs6fs7fs9fw1DC4"
+// Number 2 of fourth matrix
+
+"e00s5ds7ds8ds9ds5es7es9es5fs6fs7fs9f"
+"e0fs5ds7ds9ds5es7es9es5fs6fs7fs8fs9fw1DC4"
+// Number 3 of fourth matrix
+
+"e00s5ds7ds9ds5es7es9es5fs6fs7fs8fs9f"
+"e0fs5ds6ds7ds7es5fs6fs7fs8fs9fw1DC4"
+// Number 4 of fourth matrix
+
+"e00s5ds6ds7ds7es5fs6fs7fs8fs9f"
+"e0fs5ds6ds7ds9ds5es7es9es5fs7fs8fs9fw1DC4"
+// Number 5 of fourth matrix
+
+"e00s5ds6ds7ds9ds5es7es9es5fs7fs8fs9f"
+"e0fs5ds6ds7ds8ds9ds5es7es9es5fs7fs8fs9fw1DC4"
+// Number 6 of fourth matrix
+
+"e00s5ds6ds7ds8ds9ds5es7es9es5fs7fs8fs9f"
+"e0fs5ds5es5fs6fs7fs8fs9fw1DC4"
+// Number 7 of fourth matrix
+
+"e00s5ds5es5fs6fs7fs8fs9f"
+"e0fs5ds6ds7ds8ds9ds5es7es9es5fs6fs7fs8fs9fw1DC4"
+// Number 8 of fourth matrix
+
+"e00s5ds6ds7ds8ds9ds5es7es9es5fs6fs7fs8fs9f"
+"e0fs5ds6ds7ds9ds5es7es9es5fs6fs7fs8fs9fw1DC4"
+// Number 9 of fourth matrix
+
+"e00s59s69s79s89s99s5as9as5bs6bs7bs8bs9b"
+"e0fs5bs6bs7bs8bs9b"
+// Number 1 of third matrix
+"e00s5ds6ds7ds9ds5es7es9es5fs6fs7fs8fs9f"
+"e0fs5ds6ds7ds8ds9ds5es9es5fs6fs7fs8fs9fw1DC4"
+// Number 0 of the fourth matrix
+
+"e00s5ds6ds7ds8ds9ds5es9es5fs6fs7fs8fs9f"
+"e0fs5fs6fs7fs8fs9fw1DC4"
+// Number 1 of fourth matrix
+
+"e00s5fs6fs7fs8fs9f"
+"e0fs5ds7ds8ds9ds5es7es9es5fs6fs7fs9fw1DC4"
+// Number 2 of fourth matrix
+
+"e00s5ds7ds8ds9ds5es7es9es5fs6fs7fs9f"
+"e0fs5ds7ds9ds5es7es9es5fs6fs7fs8fs9fw1DC4"
+// Number 3 of fourth matrix
+
+"e00s5ds7ds9ds5es7es9es5fs6fs7fs8fs9f"
+"e0fs5ds6ds7ds7es5fs6fs7fs8fs9fw1DC4"
+// Number 4 of fourth matrix
+
+"e00s5ds6ds7ds7es5fs6fs7fs8fs9f"
+"e0fs5ds6ds7ds9ds5es7es9es5fs7fs8fs9fw1DC4"
+// Number 5 of fourth matrix
+
+"e00s5ds6ds7ds9ds5es7es9es5fs7fs8fs9f"
+"e0fs5ds6ds7ds8ds9ds5es7es9es5fs7fs8fs9fw1DC4"
+// Number 6 of fourth matrix
+
+"e00s5ds6ds7ds8ds9ds5es7es9es5fs7fs8fs9f"
+"e0fs5ds5es5fs6fs7fs8fs9fw1DC4"
+// Number 7 of fourth matrix
+
+"e00s5ds5es5fs6fs7fs8fs9f"
+"e0fs5ds6ds7ds8ds9ds5es7es9es5fs6fs7fs8fs9fw1DC4"
+// Number 8 of fourth matrix
+
+"e00s5ds6ds7ds8ds9ds5es7es9es5fs6fs7fs8fs9f"
+"e0fs5ds6ds7ds9ds5es7es9es5fs6fs7fs8fs9fw1DC4"
+// Number 9 of fourth matrix
+
+"e00s5bs6bs7bs8bs9b"
+"e0fs59s79s89s99s5as7as9as5bs6bs7bs9b"
+// Number 2 of third matrix
+"e00s5ds6ds7ds9ds5es7es9es5fs6fs7fs8fs9f"
+"e0fs5ds6ds7ds8ds9ds5es9es5fs6fs7fs8fs9fw1DC4"
+// Number 0 of the fourth matrix
+
+"e00s5ds6ds7ds8ds9ds5es9es5fs6fs7fs8fs9f"
+"e0fs5fs6fs7fs8fs9fw1DC4"
+// Number 1 of fourth matrix
+
+"e00s5fs6fs7fs8fs9f"
+"e0fs5ds7ds8ds9ds5es7es9es5fs6fs7fs9fw1DC4"
+// Number 2 of fourth matrix
+
+"e00s5ds7ds8ds9ds5es7es9es5fs6fs7fs9f"
+"e0fs5ds7ds9ds5es7es9es5fs6fs7fs8fs9fw1DC4"
+// Number 3 of fourth matrix
+
+"e00s5ds7ds9ds5es7es9es5fs6fs7fs8fs9f"
+"e0fs5ds6ds7ds7es5fs6fs7fs8fs9fw1DC4"
+// Number 4 of fourth matrix
+
+"e00s5ds6ds7ds7es5fs6fs7fs8fs9f"
+"e0fs5ds6ds7ds9ds5es7es9es5fs7fs8fs9fw1DC4"
+// Number 5 of fourth matrix
+
+"e00s5ds6ds7ds9ds5es7es9es5fs7fs8fs9f"
+"e0fs5ds6ds7ds8ds9ds5es7es9es5fs7fs8fs9fw1DC4"
+// Number 6 of fourth matrix
+
+"e00s5ds6ds7ds8ds9ds5es7es9es5fs7fs8fs9f"
+"e0fs5ds5es5fs6fs7fs8fs9fw1DC4"
+// Number 7 of fourth matrix
+
+"e00s5ds5es5fs6fs7fs8fs9f"
+"e0fs5ds6ds7ds8ds9ds5es7es9es5fs6fs7fs8fs9fw1DC4"
+// Number 8 of fourth matrix
+
+"e00s5ds6ds7ds8ds9ds5es7es9es5fs6fs7fs8fs9f"
+"e0fs5ds6ds7ds9ds5es7es9es5fs6fs7fs8fs9fw1DC4"
+// Number 9 of fourth matrix
+
+"e00s59s79s89s99s5as7as9as5bs6bs7bs9b"
+"e0fs59s79s99s5as7as9as5bs6bs7bs8bs9b"
+// Number 3 of third matrix
+"e00s5ds6ds7ds9ds5es7es9es5fs6fs7fs8fs9f"
+"e0fs5ds6ds7ds8ds9ds5es9es5fs6fs7fs8fs9fw1DC4"
+// Number 0 of the fourth matrix
+
+"e00s5ds6ds7ds8ds9ds5es9es5fs6fs7fs8fs9f"
+"e0fs5fs6fs7fs8fs9fw1DC4"
+// Number 1 of fourth matrix
+
+"e00s5fs6fs7fs8fs9f"
+"e0fs5ds7ds8ds9ds5es7es9es5fs6fs7fs9fw1DC4"
+// Number 2 of fourth matrix
+
+"e00s5ds7ds8ds9ds5es7es9es5fs6fs7fs9f"
+"e0fs5ds7ds9ds5es7es9es5fs6fs7fs8fs9fw1DC4"
+// Number 3 of fourth matrix
+
+"e00s5ds7ds9ds5es7es9es5fs6fs7fs8fs9f"
+"e0fs5ds6ds7ds7es5fs6fs7fs8fs9fw1DC4"
+// Number 4 of fourth matrix
+
+"e00s5ds6ds7ds7es5fs6fs7fs8fs9f"
+"e0fs5ds6ds7ds9ds5es7es9es5fs7fs8fs9fw1DC4"
+// Number 5 of fourth matrix
+
+"e00s5ds6ds7ds9ds5es7es9es5fs7fs8fs9f"
+"e0fs5ds6ds7ds8ds9ds5es7es9es5fs7fs8fs9fw1DC4"
+// Number 6 of fourth matrix
+
+"e00s5ds6ds7ds8ds9ds5es7es9es5fs7fs8fs9f"
+"e0fs5ds5es5fs6fs7fs8fs9fw1DC4"
+// Number 7 of fourth matrix
+
+"e00s5ds5es5fs6fs7fs8fs9f"
+"e0fs5ds6ds7ds8ds9ds5es7es9es5fs6fs7fs8fs9fw1DC4"
+// Number 8 of fourth matrix
+
+"e00s5ds6ds7ds8ds9ds5es7es9es5fs6fs7fs8fs9f"
+"e0fs5ds6ds7ds9ds5es7es9es5fs6fs7fs8fs9fw1DC4"
+// Number 9 of fourth matrix
+
+"e00s59s79s99s5as7as9as5bs6bs7bs8bs9b"
+"e0fs59s69s79s7as5bs6bs7bs8bs9b"
+// Number 4 of third matrix
+"e00s5ds6ds7ds9ds5es7es9es5fs6fs7fs8fs9f"
+"e0fs5ds6ds7ds8ds9ds5es9es5fs6fs7fs8fs9fw1DC4"
+// Number 0 of the fourth matrix
+
+"e00s5ds6ds7ds8ds9ds5es9es5fs6fs7fs8fs9f"
+"e0fs5fs6fs7fs8fs9fw1DC4"
+// Number 1 of fourth matrix
+
+"e00s5fs6fs7fs8fs9f"
+"e0fs5ds7ds8ds9ds5es7es9es5fs6fs7fs9fw1DC4"
+// Number 2 of fourth matrix
+
+"e00s5ds7ds8ds9ds5es7es9es5fs6fs7fs9f"
+"e0fs5ds7ds9ds5es7es9es5fs6fs7fs8fs9fw1DC4"
+// Number 3 of fourth matrix
+
+"e00s5ds7ds9ds5es7es9es5fs6fs7fs8fs9f"
+"e0fs5ds6ds7ds7es5fs6fs7fs8fs9fw1DC4"
+// Number 4 of fourth matrix
+
+"e00s5ds6ds7ds7es5fs6fs7fs8fs9f"
+"e0fs5ds6ds7ds9ds5es7es9es5fs7fs8fs9fw1DC4"
+// Number 5 of fourth matrix
+
+"e00s5ds6ds7ds9ds5es7es9es5fs7fs8fs9f"
+"e0fs5ds6ds7ds8ds9ds5es7es9es5fs7fs8fs9fw1DC4"
+// Number 6 of fourth matrix
+
+"e00s5ds6ds7ds8ds9ds5es7es9es5fs7fs8fs9f"
+"e0fs5ds5es5fs6fs7fs8fs9fw1DC4"
+// Number 7 of fourth matrix
+
+"e00s5ds5es5fs6fs7fs8fs9f"
+"e0fs5ds6ds7ds8ds9ds5es7es9es5fs6fs7fs8fs9fw1DC4"
+// Number 8 of fourth matrix
+
+"e00s5ds6ds7ds8ds9ds5es7es9es5fs6fs7fs8fs9f"
+"e0fs5ds6ds7ds9ds5es7es9es5fs6fs7fs8fs9fw1DC4"
+// Number 9 of fourth matrix
+
+"e00s59s69s79s7as5bs6bs7bs8bs9b"
+"e0fs59s69s79s99s5as7as9as5bs7bs8bs9b"
+
+
+// Number 5 of third matrix
+"e00s5ds6ds7ds9ds5es7es9es5fs6fs7fs8fs9f"
+"e0fs5ds6ds7ds8ds9ds5es9es5fs6fs7fs8fs9fw1DC4"
+// Number 0 of the fourth matrix
+
+"e00s5ds6ds7ds8ds9ds5es9es5fs6fs7fs8fs9f"
+"e0fs5fs6fs7fs8fs9fw1DC4"
+// Number 1 of fourth matrix
+
+"e00s5fs6fs7fs8fs9f"
+"e0fs5ds7ds8ds9ds5es7es9es5fs6fs7fs9fw1DC4"
+// Number 2 of fourth matrix
+
+"e00s5ds7ds8ds9ds5es7es9es5fs6fs7fs9f"
+"e0fs5ds7ds9ds5es7es9es5fs6fs7fs8fs9fw1DC4"
+// Number 3 of fourth matrix
+
+"e00s5ds7ds9ds5es7es9es5fs6fs7fs8fs9f"
+"e0fs5ds6ds7ds7es5fs6fs7fs8fs9fw1DC4"
+// Number 4 of fourth matrix
+
+"e00s5ds6ds7ds7es5fs6fs7fs8fs9f"
+"e0fs5ds6ds7ds9ds5es7es9es5fs7fs8fs9fw1DC4"
+// Number 5 of fourth matrix
+
+"e00s5ds6ds7ds9ds5es7es9es5fs7fs8fs9f"
+"e0fs5ds6ds7ds8ds9ds5es7es9es5fs7fs8fs9fw1DC4"
+// Number 6 of fourth matrix
+
+"e00s5ds6ds7ds8ds9ds5es7es9es5fs7fs8fs9f"
+"e0fs5ds5es5fs6fs7fs8fs9fw1DC4"
+// Number 7 of fourth matrix
+
+"e00s5ds5es5fs6fs7fs8fs9f"
+"e0fs5ds6ds7ds8ds9ds5es7es9es5fs6fs7fs8fs9fw1DC4"
+// Number 8 of fourth matrix
+
+"e00s5ds6ds7ds8ds9ds5es7es9es5fs6fs7fs8fs9f"
+"e0fs5ds6ds7ds9ds5es7es9es5fs6fs7fs8fs9fw1DC4"
+// Number 9 of fourth matrix
+
+"e00s55s65s75s85s95s56s96s57s67s77s87s97"
+"e0fs57s67s77s87s97"
+// Number 1 of second matrix
+"e00s59s69s79s99s5as7as9as5bs7bs8bs9b"
+"e0fs59s69s79s89s99s5as9as5bs6bs7bs8bs9b"
+// Number 0 of the third matrix
+"e00s5ds6ds7ds9ds5es7es9es5fs6fs7fs8fs9f"
+"e0fs5ds6ds7ds8ds9ds5es9es5fs6fs7fs8fs9fw1DC4"
+// Number 0 of the fourth matrix
+
+"e00s5ds6ds7ds8ds9ds5es9es5fs6fs7fs8fs9f"
+"e0fs5fs6fs7fs8fs9fw1DC4"
+// Number 1 of fourth matrix
+
+"e00s5fs6fs7fs8fs9f"
+"e0fs5ds7ds8ds9ds5es7es9es5fs6fs7fs9fw1DC4"
+// Number 2 of fourth matrix
+
+"e00s5ds7ds8ds9ds5es7es9es5fs6fs7fs9f"
+"e0fs5ds7ds9ds5es7es9es5fs6fs7fs8fs9fw1DC4"
+// Number 3 of fourth matrix
+
+"e00s5ds7ds9ds5es7es9es5fs6fs7fs8fs9f"
+"e0fs5ds6ds7ds7es5fs6fs7fs8fs9fw1DC4"
+// Number 4 of fourth matrix
+
+"e00s5ds6ds7ds7es5fs6fs7fs8fs9f"
+"e0fs5ds6ds7ds9ds5es7es9es5fs7fs8fs9fw1DC4"
+// Number 5 of fourth matrix
+
+"e00s5ds6ds7ds9ds5es7es9es5fs7fs8fs9f"
+"e0fs5ds6ds7ds8ds9ds5es7es9es5fs7fs8fs9fw1DC4"
+// Number 6 of fourth matrix
+
+"e00s5ds6ds7ds8ds9ds5es7es9es5fs7fs8fs9f"
+"e0fs5ds5es5fs6fs7fs8fs9fw1DC4"
+// Number 7 of fourth matrix
+
+"e00s5ds5es5fs6fs7fs8fs9f"
+"e0fs5ds6ds7ds8ds9ds5es7es9es5fs6fs7fs8fs9fw1DC4"
+// Number 8 of fourth matrix
+
+"e00s5ds6ds7ds8ds9ds5es7es9es5fs6fs7fs8fs9f"
+"e0fs5ds6ds7ds9ds5es7es9es5fs6fs7fs8fs9fw1DC4"
+// Number 9 of fourth matrix
+
+"e00s59s69s79s89s99s5as9as5bs6bs7bs8bs9b"
+"e0fs5bs6bs7bs8bs9b"
+// Number 1 of third matrix
+"e00s5ds6ds7ds9ds5es7es9es5fs6fs7fs8fs9f"
+"e0fs5ds6ds7ds8ds9ds5es9es5fs6fs7fs8fs9fw1DC4"
+// Number 0 of the fourth matrix
+
+"e00s5ds6ds7ds8ds9ds5es9es5fs6fs7fs8fs9f"
+"e0fs5fs6fs7fs8fs9fw1DC4"
+// Number 1 of fourth matrix
+
+"e00s5fs6fs7fs8fs9f"
+"e0fs5ds7ds8ds9ds5es7es9es5fs6fs7fs9fw1DC4"
+// Number 2 of fourth matrix
+
+"e00s5ds7ds8ds9ds5es7es9es5fs6fs7fs9f"
+"e0fs5ds7ds9ds5es7es9es5fs6fs7fs8fs9fw1DC4"
+// Number 3 of fourth matrix
+
+"e00s5ds7ds9ds5es7es9es5fs6fs7fs8fs9f"
+"e0fs5ds6ds7ds7es5fs6fs7fs8fs9fw1DC4"
+// Number 4 of fourth matrix
+
+"e00s5ds6ds7ds7es5fs6fs7fs8fs9f"
+"e0fs5ds6ds7ds9ds5es7es9es5fs7fs8fs9fw1DC4"
+// Number 5 of fourth matrix
+
+"e00s5ds6ds7ds9ds5es7es9es5fs7fs8fs9f"
+"e0fs5ds6ds7ds8ds9ds5es7es9es5fs7fs8fs9fw1DC4"
+// Number 6 of fourth matrix
+
+
+
+
+
---
+++ README.md
@@ -0,0 +1,5 @@
+ This is all the coding contents that I have done for the project work course at Tampere University
+
+The project is working in ATMEL Studio 7.0
+
+I'm responsible for Archive folder + animaatio.h file, mainly control the animation of the LED blinker.
